68a9358c412edcf319a1dbf8784675ba36acbad9
Eric <ericspsantos@gmail.com>
2023-09-29 02:55:05 -0300

    deletando arquivos desnecessarios

--- API-WEB-SAUDE/src/middlewares/multer.ts
+++
@@ -1,7 +0,0 @@
-import multer from 'multer';
-
-// Configurar o middleware do multer para processar o upload do arquivo
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-export default upload;
