ed5433f41c5c2bcd1a842441a457afbc877a5bcb
Eric <ericspsantos@gmail.com>
2023-10-08 00:01:41 -0300

    readme

--- API-WEB-SAUDE/src/routes/filtroRoute.ts
+++ API-WEB-SAUDE/src/routes/filtroRoute.ts
@@ -20,26 +20,5 @@ filtroRouter.get('/buscar/', async (req: Request, res: Response) => {
 		return res.json(error);
 	}
 });
-filtroRouter.get('/clinicas/especialidade/:especialidade', async (req, res) => {
-	try {
-		const { especialidade } = req.params; // Obtém a especialidade dos parâmetros da URL
 
-		const clinicasEhospitais =
-			await FiltroRepository.pegarClinicasPorEspecialidade(especialidade);
-
-		if (!clinicasEhospitais || clinicasEhospitais.length === 0) {
-			return res
-				.status(404)
-				.json({
-					Message:
-						'Nenhuma clínica ou hospital encontrada para a especialidade fornecida!',
-				});
-		}
-
-		return res.status(200).json(clinicasEhospitais);
-	} catch (error) {
-		console.error(error);
-		return res.status(500).json(error);
-	}
-});
 export default filtroRouter;
